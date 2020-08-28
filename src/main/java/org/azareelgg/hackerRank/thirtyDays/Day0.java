568b004582021e0996e04445d0a23290e567e989
azareelgg <azareelgg@gmail.com>
2020-08-27 22:06:16 -0500

    Revert "day0"
    This reverts commit c70872deedd086d064ad5102b7e53f6571958f32.

--- src/main/java/org/azareelgg/hackerRank/thirtyDays/Day0.java
+++
@@ -1,9 +0,0 @@
-package org.azareelgg.hackerRank.thirtyDays;
-
-public class Day0 {
-    
-    void day0(){
-        System.out.println("Hello, World.");
-        System.out.println("Welcome to 30 Days of Code!");
-    }
-}
