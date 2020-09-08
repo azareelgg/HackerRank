4e059f67b863126de7bb8260929d3874ee8208be
Azareel Gonzalez <azareelgg@gmail.com>
2020-09-07 20:46:00 -0500

    Day01: Data Types - improvement.

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day01.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day01.java
@@ -30,7 +30,7 @@ public class Day01 {
         /* Print the sum of the double variables on a new line. */
 	System.out.println(d+d2);
         /* Concatenate and print the String variables on a new line; 
-        	the 's' variable above should be printed first. */
+        the 's' variable above should be printed first. */
         System.out.println(s + s2);
         scan.close();
     }
