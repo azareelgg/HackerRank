d28b1dfa3a5d5a7d57d4a442e011e7d3963a57ca
Azareel Gonzalez <azareelgg@gmail.com>
2020-09-09 23:21:27 -0500

    Day 03: Intro to Conditional Statements

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day03.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day03.java
@@ -1,9 +1,28 @@
 package com.azareelgg.thirtydaysofcode;
 
+import java.util.Scanner;
+
 /**
  *
  * @author azareel
  */
 public class Day03 {
-    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N % 2 == 0){//even
+            if(N >= 2 && N <= 5){
+                System.out.println("Not Weird");
+            }else if(N >= 6 && N <= 20){
+                System.out.println("Weird");
+            }else if(N >= 20){
+                System.out.println("Not Weird");
+            }
+        }else{//odd
+            System.out.println("Weird");
+        }
+        scanner.close();
+    }
 }
