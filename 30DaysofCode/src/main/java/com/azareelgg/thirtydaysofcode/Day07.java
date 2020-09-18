8e369dcaa849611030d71a46a7b6a6f608d000c7
Azareel Gonzalez <azareelgg@gmail.com>
2020-09-17 22:27:06 -0500

    Day 08: Dictionaries and Maps

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day07.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day07.java
@@ -1,4 +1,27 @@
 package com.azareelgg.thirtydaysofcode;
 
+import java.util.Scanner;
+
 public class Day07 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+        for(int j=arr.length; j>0; j--){
+            System.out.print(arr[j-1]+" ");
+        }
+        scanner.close();
+    }
 }
--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day08.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day08.java
@@ -1,4 +1,28 @@
 package com.azareelgg.thirtydaysofcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Day08 {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            map.put(name, phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            int i = map.getOrDefault(s, 0);
+            if(i != 0){
+                System.out.println(s+"="+i);
+            }else{
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+    }
 }
