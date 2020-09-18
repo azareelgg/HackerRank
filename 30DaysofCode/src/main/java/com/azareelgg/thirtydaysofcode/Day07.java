09095917c1dd5dd737b024c319d42235273243f4
Azareel Gonzalez <azareelgg@gmail.com>
2020-09-17 22:28:41 -0500

    Day 07: Arrays

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day07.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day07.java
@@ -7,6 +7,7 @@ public class Day07 {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
