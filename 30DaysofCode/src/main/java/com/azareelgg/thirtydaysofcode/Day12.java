a89bce89db7f5d3147c05339742d6ddff02f17d8
Azareel Gonzalez <azareelgg@gmail.com>
2020-09-23 23:54:31 -0500

    Day 12: Inheritance

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day12.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day12.java
@@ -1,5 +1,27 @@
 package com.azareelgg.thirtydaysofcode;
 
-public class Day12 {
+import java.util.Scanner;
 
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
+
+    public Person(){
+
+    }
+
+    // Constructor
+    Person(String firstName, String lastName, int identification){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+
+    // Print person data
+    public void printPerson(){
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + 	"\nID: " + idNumber);
+    }
 }
