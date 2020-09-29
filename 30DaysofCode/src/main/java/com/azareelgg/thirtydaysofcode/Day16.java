08ca750688da85e60f4f3e95d72c130c2ddbe480
Azareel Gonzalez <azareelgg@gmail.com>
2020-09-29 10:02:02 -0500

    Day 16: Exceptions - String to Integer

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day16.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day16.java
@@ -3,7 +3,6 @@ package com.azareelgg.thirtydaysofcode;
 import java.util.Scanner;
 
 public class Day16 {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String S = in.next();
