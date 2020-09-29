b30dc869b4939f9c23405c1850faa631e623dd09
Azareel Gonzalez <azareelgg@gmail.com>
2020-09-29 09:49:30 -0500

    Day 15: Linked List

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day16.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day16.java
@@ -1,5 +1,17 @@
 package com.azareelgg.thirtydaysofcode;
 
+import java.util.Scanner;
+
 public class Day16 {
 
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try{
+            int tmp = Integer.parseInt(S);
+            System.out.println(tmp);
+        }catch(Exception e){
+            System.out.println("Bad String");
+        }
+    }
 }
