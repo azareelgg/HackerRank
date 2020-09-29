83c126f96fa4e3113b439664e5690587cb73c9e1
Azareel Gonzalez <azareelgg@gmail.com>
2020-09-28 19:23:14 -0500

    Day 15: Linked List

---
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day15.java
@@ -0,0 +1,41 @@
+package com.azareelgg.thirtydaysofcode;
+
+import java.util.Scanner;
+
+class Day15Node{
+    int data;
+    Day15Node next;
+    Day15Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class Day15 {
+    public static Day15Node insert(Day15Node head, int data) {
+        if (head == null) return new Day15Node(data);
+        head.next = insert(head.next, data);
+        return head;
+    }
+
+    public static void display(Day15Node head) {
+        Day15Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Day15Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
