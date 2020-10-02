79d1f21f37f045c1c9dce8d7b9337dfec5c486aa
Azareel Gonzalez <azareelgg@gmail.com>
2020-10-02 13:31:53 -0500

    Day 20: Sorting

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day20.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day20.java
@@ -10,6 +10,33 @@ public class Day20 {
         for(int a_i=0; a_i < n; a_i++){
             a[a_i] = in.nextInt();
         }
-        // Write Your Code Here
+        int ns = bubbleSort(a);
+        System.out.println("Array is sorted in "+ns+" swaps.");
+        System.out.println("First Element: "+a[0]);
+        System.out.println("Last Element: "+a[a.length - 1]);
+    }
+
+    static int bubbleSort(int[] a){
+        int n = a.length;
+        int numberOfSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    numberOfSwaps++;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+        return numberOfSwaps;
     }
 }
