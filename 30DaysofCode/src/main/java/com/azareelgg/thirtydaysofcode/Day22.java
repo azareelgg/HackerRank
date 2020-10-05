07227b441a3c81663bfdf1d76e429096c3fae4db
Azareel Gonzalez <azareelgg@gmail.com>
2020-10-05 12:54:23 -0500

    Day 22: Binary Search Trees

--- 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day22.java
+++ 30DaysofCode/src/main/java/com/azareelgg/thirtydaysofcode/Day22.java
@@ -1,7 +1,54 @@
 package com.azareelgg.thirtydaysofcode;
 
-public class Day22 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+class NodeDay22 {
+    NodeDay22 left,right;
+    int data;
+    NodeDay22(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+
+public class Day22 {
+
+    public static int getHeight(NodeDay22 root){
+        if(root == null) {
+            return -1;
+        }
+        int leftDepth = getHeight(root.left);
+        int rightDepth = getHeight(root.right);
+
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+
+    public static NodeDay22 insert(NodeDay22 root, int data){
+        if(root==null){
+            return new NodeDay22(data);
+        }
+        else{
+            NodeDay22 cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        NodeDay22 root=null;
+        while(T-->0){
+            int data=sc.nextInt();
+            root=insert(root,data);
+        }
+        int height=getHeight(root);
+        System.out.println(height);
     }
 }
