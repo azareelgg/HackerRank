c1adceba72a3c41f5835200428afb3ee8a6cae9e
Azareel Gonzalez <azareelgg@gmail.com>
2024-09-24 23:28:59 -0600

    Miscellaneous

---
+++ problem-solving/src/main/java/com/azareelgg/hackerRank/interviewPreparation/miscellaneous/ByteConstructor.java
@@ -0,0 +1,22 @@
+package com.azareelgg.hackerRank.interviewPreparation.miscellaneous;
+
+public class ByteConstructor {
+
+	ByteConstructor(int a, int b){
+		System.out.println("a = " + a + " b = " + b);
+	}
+
+	ByteConstructor(int a, float b){
+		System.out.println("a = " + a + " b = " + b);
+	}
+	
+	public static void main(String[] args) {
+
+		byte a = 10;
+		byte b = 15;
+
+		ByteConstructor test = new ByteConstructor(a,b);
+	}
+	
+	
+}
